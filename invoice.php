cade4f8839b4c23c4a946125b5d0cc5445cacd66
Smart Sender <92857429+smartsendercommunity@users.noreply.github.com>
2023-05-02 15:05:46 +0300

    Update invoice.php

--- invoice.php
+++ invoice.php
@@ -21,6 +21,17 @@ http_response_code(200);
 
 $inputJSON = file_get_contents('php://input');
 
+function send_bearer($url, $token, $type = "GET", $param = []){
+    $descriptor = curl_init($url);
+     curl_setopt($descriptor, CURLOPT_POSTFIELDS, json_encode($param));
+     curl_setopt($descriptor, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($descriptor, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer '.$token)); 
+     curl_setopt($descriptor, CURLOPT_CUSTOMREQUEST, $type);
+    $itog = curl_exec($descriptor);
+    curl_close($descriptor);
+    return $itog;
+}
+
 // Подмена переменных
 $userId = $_GET["userId"];
 $ssToken = $_GET["token"];
@@ -64,37 +75,15 @@ if ($l > 0) {
 return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
 }
 function send_forward($inputJSON, $link){
-	
-$request = 'POST';	
-		
-$descriptor = curl_init($link);
-
- curl_setopt($descriptor, CURLOPT_POSTFIELDS, $inputJSON);
- curl_setopt($descriptor, CURLOPT_RETURNTRANSFER, 1);
- curl_setopt($descriptor, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
- curl_setopt($descriptor, CURLOPT_CUSTOMREQUEST, $request);
-
+    $request = 'POST';	
+    $descriptor = curl_init($link);
+     curl_setopt($descriptor, CURLOPT_POSTFIELDS, $inputJSON);
+     curl_setopt($descriptor, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($descriptor, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+     curl_setopt($descriptor, CURLOPT_CUSTOMREQUEST, $request);
     $itog = curl_exec($descriptor);
     curl_close($descriptor);
-
-   		 return $itog;
-		
-}
-function send_bearer($url, $token, $type = "GET", $param = []){
-	
-		
-$descriptor = curl_init($url);
-
- curl_setopt($descriptor, CURLOPT_POSTFIELDS, json_encode($param));
- curl_setopt($descriptor, CURLOPT_RETURNTRANSFER, 1);
- curl_setopt($descriptor, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer '.$token)); 
- curl_setopt($descriptor, CURLOPT_CUSTOMREQUEST, $type);
-
-    $itog = curl_exec($descriptor);
-    curl_close($descriptor);
-
-   		 return $itog;
-		
+    return $itog;
 }
 }
 
